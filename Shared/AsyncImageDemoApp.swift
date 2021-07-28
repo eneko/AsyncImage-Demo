5da6c2a07960baae9cb0332088a9a25ed0811f82
Eneko Alonso <eneko.alonso@gmail.com>
2021-07-27 18:11:01 -0700

    Initial Commit

---
+++ Shared/AsyncImageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageDemoApp.swift
+//  Shared
+//
+//  Created by Eneko Alonso on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
